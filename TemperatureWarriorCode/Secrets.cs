5b0b51513910692564000165d65180aa0bca063b
Isaac <isaacbg2002@gmail.com>
2024-03-18 15:16:17 +0100

    Archivos iniciales

---
+++ TemperatureWarriorCode/Secrets.cs
@@ -0,0 +1,10 @@
+﻿namespace TemperatureWarriorCode {
+    internal class Secrets {
+
+        //WiFi NAME 
+        public const string WIFI_NAME = "Phone_1_8316";
+
+        //PASSWORD FOR THE WiFi NAME
+        public const string WIFI_PASSWORD = "patata02";
+    }
+}
