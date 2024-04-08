6883de4ed6a86f290b5deb9edd1179a47023d751
FerGalan19 <99019697+FerGalan19@users.noreply.github.com>
2024-04-08 17:06:51 +0200

    código rele

---
+++ TemperatureWarriorCode/rele.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+
+namespace TemperatureWarriorCode
+{
+    public class Rele
+    {
+        // Define the pin that the relay is connected to
+        IDigitalOutputPort relayPort;
+
+       // Constructor
+         public Rele(IDigitalOutputPort relayPort)
+         {
+              this.relayPort = relayPort;
+         }
+        
+        // Function to turn on the relay
+        public async Task TurnOn()
+        {
+            relayPort.State = true;
+            Console.WriteLine("Relay On");
+        }
+
+        // Function to turn off the relay
+        public async Task TurnOff()
+        {
+            relayPort.State = false;
+            Console.WriteLine("Relay Off");
+        }
+
+        // Function to ivert the polarity of the relay
+        public async Task Invert()
+        {
+            relayPort.State = !relayPort.State;
+            Console.WriteLine("Relay Inverted");
+        }
+    }
+}
+// Path: TemperatureWarriorCode/Rele.cs
