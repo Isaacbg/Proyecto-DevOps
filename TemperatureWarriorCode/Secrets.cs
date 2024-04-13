5e789ed5e1b9a1fac5b21f30df683dd287d542a5
valentinacataldo <valentina.cataldo@adisic.com>
2024-04-14 00:23:25 +0200

    Inicio refactor de interfaz

--- TemperatureWarriorCode/Secrets.cs
+++ TemperatureWarriorCode/Secrets.cs
@@ -2,9 +2,9 @@
     internal class Secrets {
 
         //WiFi NAME 
-        public const string WIFI_NAME = "Phone_1_8316";
+        public const string WIFI_NAME = "OPPO Reno2 Z";
 
         //PASSWORD FOR THE WiFi NAME
-        public const string WIFI_PASSWORD = "patata02";
+        public const string WIFI_PASSWORD = "9e2e032237b8";
     }
 }
